32a6aee30734b4ef0b7ce30ffe9d1813de414642
joao <joao.camargo@gmail.com>
2021-06-10 16:52:15 -0300

    init

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by joao camargo on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
